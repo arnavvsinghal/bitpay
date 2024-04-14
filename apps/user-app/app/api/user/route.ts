814a664de6e558b592868d9fc36bc1f40af99b21
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-04-14 19:58:57 +0530

    init: :tada: initialise bitpay

---
+++ apps/user-app/app/api/user/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server"
+import { PrismaClient } from "@bitpay/db/client";
+
+const client = new PrismaClient();
+
+export const GET = async () => {
+    await client.user.create({
+        data: {
+            email: "abc",
+            name: "abc"
+        }
+    })
+    return NextResponse.json({
+        message: "hi there"
+    })
+}
---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page(): JSX.Element {
+    return (
+      <div className="text-red-500">
+        hello
+      </div>
+    );
+  }
+  
