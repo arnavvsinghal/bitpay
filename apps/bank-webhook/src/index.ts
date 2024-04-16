0720f3a5ca500f7c8ee6ed93e78aa43b7eef85fa
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-04-16 21:35:02 +0530

    feat(bank-webhook): :lock: add transaction for Balance and onRampTransaction update

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,13 +1,50 @@
 import express from "express";
-
+import db from "@bitpay/db/client";
 const app = express();
 
-app.post("/hdfcWebhook", (req, res) => {
-    //TODO: Add zod validation here?
-    const paymentInformation = {
-        token: req.body.token,
-        userId: req.body.user_identifier,
-        amount: req.body.amount
+app.use(express.json());
+
+app.post("/hdfcWebhook", async (req, res) => {
+  //TODO: Add zod validation here?
+  const body = req.body;
+
+  const paymentInformation: { token: string; userId: number; amount: number } =
+    {
+      token: body.token,
+      userId: Number(body.user_identifier),
+      amount: Number(body.amount),
     };
-    // Update balance in db, add txn
-})
+  try {
+    await db.$transaction([
+      db.balance.update({
+        where: {
+          userId: paymentInformation.userId,
+        },
+        data: {
+          amount: { increment: paymentInformation.amount },
+        },
+      }),
+      db.onRampTransaction.update({
+        where: {
+          token: paymentInformation.token,
+        },
+        data: {
+          status: "Success",
+        },
+      }),
+    ]);
+    res.json({
+      message: "captured",
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(411).json({
+      message: "Error while processing webhook",
+    });
+  }
+});
+
+const PORT = 3003;
+app.listen(PORT, () => {
+  `WebHook is listening on ${PORT}`;
+});
