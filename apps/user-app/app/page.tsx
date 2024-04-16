0186b7d3ce3a99eaef14a95d998bdea9533f90f4
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-04-16 18:16:48 +0530

    feat(user-app): :sparkles: signin signout functionality

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,10 +1,12 @@
-"use client";
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Appbar } from "@repo/ui/Appbar";
 
-import {useBalance} from "@bitpay/store/useBalance";
-
-export default function() {
-  const balance = useBalance();
-  return <div>
-    hi there {balance}
-  </div>
+export default function Page(): JSX.Element {
+  const session = useSession();
+  return (
+   <div>
+      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+   </div>
+  );
 }
