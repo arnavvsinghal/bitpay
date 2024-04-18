99235458f58ab79ee1936d2acaaa42059da3e12d
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-04-18 12:34:43 +0530

    feat(landing-page): :sparkles: create landing page template

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,12 +1,27 @@
 import { getServerSession } from "next-auth";
-import { redirect } from 'next/navigation'
+import { redirect } from "next/navigation";
 import { authOptions } from "./lib/auth";
+import { Center } from "@repo/ui/Center";
+import { HorizontalScroll } from "../components/HorizontalScroll";
+import { StatsCard } from "../components/StatsCard";
+import { Boxes } from "../components/Box";
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
-  if (session?.user) {
-    redirect('/dashboard')
-  } else {
-    redirect('/api/auth/signin')
+  console.log(session);
+  if (session) {
+    redirect("/dashboard");
   }
+  return (
+    <div>
+      <Boxes hoverColor="bg-bgsecondary"/>
+      <div className="h-screen bg-bgprimary">
+        <Center>
+          <div className="text-7xl text-textprimary z-10">Hi</div>
+        </Center>
+      </div>
+      <HorizontalScroll />
+      <StatsCard />
+    </div>
+  );
 }
