b42e82eeb70bd423da0a44fb1fd2cd8689b6f1d5
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-04-18 23:28:22 +0530

    feat(user-app): :sparkles: update landing page template

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -3,8 +3,8 @@ import { redirect } from "next/navigation";
 import { authOptions } from "./lib/auth";
 import { Center } from "@repo/ui/Center";
 import { HorizontalScroll } from "../components/HorizontalScroll";
-import { StatsCard } from "../components/StatsCard";
 import { Boxes } from "../components/Box";
+import StickyScroll from "../components/StickyScroll";
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
@@ -14,14 +14,14 @@ export default async function Page() {
   }
   return (
     <div>
-      <Boxes hoverColor="bg-bgsecondary"/>
-      <div className="h-screen bg-bgprimary">
+      <div className="h-screen relative w-full overflow-hidden flex flex-col items-center justify-center rounded-lg bg-bgprimary">
+        <Boxes />
         <Center>
           <div className="text-7xl text-textprimary z-10">Hi</div>
         </Center>
       </div>
       <HorizontalScroll />
-      <StatsCard />
+      <StickyScroll />
     </div>
   );
 }
