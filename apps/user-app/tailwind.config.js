069a3374bbb7af263dd2b47a2dad5e9925a07ccf
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-04-18 12:31:38 +0530

    feat(ui): :lipstick: add user-app tailwind color variables

--- apps/user-app/tailwind.config.js
+++ apps/user-app/tailwind.config.js
@@ -4,10 +4,19 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}"
+    "../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgprimary: "hsl(0, 0%, 10%)",
+        bgsecondary: "hsla(0, 0%, 10%,0.7)",
+        textprimary: "hsl(0, 0%, 100%);",
+        textsecondary: "hsla(0, 0%, 100%, 0.9)",
+        texttertiary: "hsla(0, 0%, 100%, 0.7)",
+        accentprimary: "hsl(196, 100%, 50%)",
+      },
+    },
   },
   plugins: [],
-}
+};
