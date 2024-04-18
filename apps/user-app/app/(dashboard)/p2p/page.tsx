3ccc7391ae218da4a55f32dd240f8415908a6dd5
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-04-18 08:32:14 +0530

    feat(p2p): :sparkles: add SendCard to p2p page

--- apps/user-app/app/(dashboard)/p2p/page.tsx
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -1,5 +1,7 @@
+import { SendCard } from "../../../components/SendCard";
+
 export default function() {
-    return <div>
-        p2p
+    return <div className="w-full">
+        <SendCard />
     </div>
 }
