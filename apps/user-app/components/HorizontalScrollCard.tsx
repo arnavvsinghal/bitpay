5afde312231529b6e651b6fce75778b659c87967
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-04-19 18:08:56 +0530

    feat(scroll-card): :lipstick: encapsulate card information in horizontal card component

--- apps/user-app/components/HorizontalScrollCard.tsx
+++ apps/user-app/components/HorizontalScrollCard.tsx
@@ -1,14 +1,22 @@
 "use client";
 // @refresh reset
 import { useScroll, useTransform, motion, useInView } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 // @refresh reset
 export const HorizontalScrollCard = ({
   id,
+  heading,
+  content,
 }: {
   id: number;
+  heading: string;
+  content: string;
 }) => {
   const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+  });
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
   const isInView = useInView(ref);
   const colours = [
     "hsla(0, 0%, 100%, 0.9)",
@@ -18,14 +26,20 @@ export const HorizontalScrollCard = ({
   return (
     <motion.div
       ref={ref}
-      initial={{ background: `${colours[id]}`, padding: "10px" }}
-      // animate={
-      //   isInView
-      //     ? { background: "pink" }
-      //     : { background: "green" }
-      // }
+      initial={{ background: `${colours[id]}` }}
       transition={{ duration: 4 }}
       className="h-screen w-screen bg-black p-1"
-    />
+    >
+      <div
+        className={`text-9xl ${id % 2 ? "text-textsecondary" : "text-bgprimary"}`}
+      >
+        {heading}
+      </div>
+      <div
+        className={`text-2xl ${id % 2 ? "text-texttertiary" : "text-bgprimary"}`}
+      >
+        {content}
+      </div>
+    </motion.div>
   );
 };
