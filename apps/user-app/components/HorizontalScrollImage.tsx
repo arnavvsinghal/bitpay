88e5ca0b0dbd40ea180a405c3e965dd9728a7420
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-04-20 09:54:02 +0530

    feat(scroll-image): :sparkles: change svg images

--- apps/user-app/components/HorizontalScrollImage.tsx
+++ apps/user-app/components/HorizontalScrollImage.tsx
@@ -5,26 +5,26 @@ const HorizontalScrollImage = ({ src }: { src: number }) => {
       {src == 0 ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
+          fill="none"
           viewBox="0 0 24 24"
-          fill="#85BB65"
-          className={`${imgClass}`}
+          strokeWidth={1.25}
+          stroke="hsl(0, 0%, 10%)"
+          className="w-full h-full"
         >
-          <path d="M12 7.5a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Z" />
           <path
-            fillRule="evenodd"
-            d="M1.5 4.875C1.5 3.839 2.34 3 3.375 3h17.25c1.035 0 1.875.84 1.875 1.875v9.75c0 1.036-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 0 1 1.5 14.625v-9.75ZM8.25 9.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM18.75 9a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V9.75a.75.75 0 0 0-.75-.75h-.008ZM4.5 9.75A.75.75 0 0 1 5.25 9h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H5.25a.75.75 0 0 1-.75-.75V9.75Z"
-            clipRule="evenodd"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3"
           />
-          <path d="M2.25 18a.75.75 0 0 0 0 1.5c5.4 0 10.63.722 15.6 2.075 1.19.324 2.4-.558 2.4-1.82V18.75a.75.75 0 0 0-.75-.75H2.25Z" />
         </svg>
       ) : src == 1 ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="hsl(196, 100%, 50%)"
-          className={`${imgClass}`}
+          strokeWidth={1.25}
+          stroke="hsla(0, 0%, 100%, 0.9)"
+          className="w-full h-full"
         >
           <path
             strokeLinecap="round"
@@ -37,9 +37,9 @@ const HorizontalScrollImage = ({ src }: { src: number }) => {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="#FFD700"
-          className={`${imgClass}`}
+          strokeWidth={1.25}
+          stroke="hsl(0, 0%, 10%)"
+          className="h-full w-full"
         >
           <path
             strokeLinecap="round"
