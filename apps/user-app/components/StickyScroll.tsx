facd7a7aa27c0ab92e72f4ee06f8c75d608cb942
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-04-20 09:47:33 +0530

    feat(sticky-scroll): :lipstick: add content to cards

--- apps/user-app/components/StickyScroll.tsx
+++ apps/user-app/components/StickyScroll.tsx
@@ -1,17 +1,39 @@
-"use client"
+"use client";
 // @refresh reset
 import { motion } from "framer-motion";
+import StickyScrollHeading from "./StickyScrollHeading";
+import { AnimatedNumber } from "./AnimatedNumber";
 // @refresh reset
 const StickyScroll = () => {
-  const colours = ["bg-accentprimary", "bg-accentsecondary", "bg-accenttertiary", "bg-accentquaternary","bg-bgprimary"];
+  const colours = [
+    "bg-accentprimary",
+    "bg-accentsecondary",
+    "bg-accenttertiary",
+    "bg-accentquaternary",
+    "bg-bgprimary",
+  ];
   return (
     <div>
-      <motion.div className={`h-screen sticky top-0 ${colours[0]}`}/>
-      <motion.div className={`h-screen sticky top-32 ${colours[1]}`}/>
-      <motion.div className={`h-screen sticky top-64 ${colours[2]}`}/>
-      <motion.div className={`h-screen sticky top-96 ${colours[3]}`}/>
-      <motion.div className={`h-screen sticky top-[512px] ${colours[4]}`}/>
-      <motion.div className={`h-screen sticky ${colours[4]}`}/>
+      <motion.div className={`h-screen w-screen sticky top-0 ${colours[0]}`}>
+        <StickyScrollHeading>WITH BITPAY YOU CAN</StickyScrollHeading>
+      </motion.div>
+      <motion.div className={`h-screen sticky top-32 ${colours[1]}`}>
+        <StickyScrollHeading>MAKE PAYMENTS</StickyScrollHeading>
+      </motion.div>
+      <motion.div className={`h-screen sticky top-64 ${colours[2]}`}>
+        <StickyScrollHeading>ANALYSE EXPENDITURE</StickyScrollHeading>
+      </motion.div>
+      <motion.div className={`h-screen sticky top-96 ${colours[3]}`}>
+        <StickyScrollHeading>MAKE WITHDRAWALS</StickyScrollHeading>
+      </motion.div>
+      <motion.div className={`h-screen sticky top-[512px] ${colours[4]}`}>
+        <StickyScrollHeading>AND MUCH MORE!</StickyScrollHeading>
+      </motion.div>
+      <motion.div className={`h-screen sticky top-[512px] ${colours[4]}`}>
+        <StickyScrollHeading>
+          <AnimatedNumber value={1000} />
+        </StickyScrollHeading>
+      </motion.div>
     </div>
   );
 };
