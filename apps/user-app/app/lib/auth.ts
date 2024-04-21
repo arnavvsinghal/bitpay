e135bbb52f2c5133d1ec4b13d378022fc010b49c
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-04-21 15:58:32 +0530

    feat(db): :card_file_box: add user to db on signin

--- apps/user-app/app/lib/auth.ts
+++ apps/user-app/app/lib/auth.ts
@@ -21,101 +21,27 @@ export const authOptions = {
       session.user.id = token.sub;
       return session;
     },
-    async signIn(a:any) {
-      console.log(a)
+    async signIn({ user }: any) {
+      if (!user?.email) {
+        throw new Error("No profile");
+      }
+      await db.user.upsert({
+        where: {
+          email: user.email,
+        },
+        create: {
+          email: user.email,
+          name: user.name,
+        },
+        update: {
+          name: user.name,
+        },
+      });
       return true;
-      // if (!profile?.email) {
-      //   throw new Error('No profile')
-      // }
-
-      // await prisma.user.upsert({
-      //   where: {
-      //     email: profile.email,
-      //   },
-      //   create: {
-      //     email: profile.email,
-      //     name: profile.name,
-      //   },
-      //   update: {
-      //     name: profile.name,
-      //   },
-      // })
-      // return true
     },
   },
- 
+
   pages: {
     signIn: "/signin",
   },
-  //   CredentialsProvider({
-  //     name: "Credentials",
-  //     credentials: {
-  //       name: {
-  //         label: "Name",
-  //         type: "text",
-  //         placeholder: "tyler durden",
-  //       },
-  //       email: {
-  //         label: "Email",
-  //         type: "text",
-  //         placeholder: "projectmayhem@fc.com",
-  //       },
-  //       phone: {
-  //         label: "Phone number",
-  //         type: "text",
-  //         placeholder: "1231231231",
-  //       },
-  //       password: {
-  //         label: "Password",
-  //         type: "password",
-  //         placeholder: "••••••••",
-  //       },
-  //     },
-  //     async authorize(credentials: any) {
-  //       const existingUser = await db.user.findFirst({
-  //         where: {
-  //           number: credentials.phone,
-  //           email: credentials.email,
-  //         },
-  //       });
-
-  //       if (existingUser) {
-  //         const passwordValidation = await bcrypt.compare(
-  //           credentials.password,
-  //           existingUser.password
-  //         );
-  //         if (passwordValidation) {
-  //           return {
-  //             id: existingUser.id.toString(),
-  //             name: existingUser.name,
-  //             email: existingUser.number,
-  //           };
-  //         }
-  //         return null;
-  //       }
-
-  //       const hashedPassword = await bcrypt.hash(credentials.password, 10);
-  //       try {
-  //         const user = await db.user.create({
-  //           data: {
-  //             email: credentials.email,
-  //             name: credentials.name,
-  //             number: credentials.phone,
-  //             password: hashedPassword,
-  //           },
-  //         });
-
-  //         return {
-  //           id: user.id.toString(),
-  //           name: user.name,
-  //           email: user.number,
-  //         };
-  //       } catch (e) {
-  //         console.error(e);
-  //       }
-
-  //       return null;
-  //     },
-  //   }),
-  // ],
 };
