ecaaa62b6d93566f912b5022176351b9d46887ab
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-04-22 11:04:10 +0530

    feat(sidebar): :sparkles: update sidebar styling and add highlight selected tab animation

---
+++ apps/user-app/components/Sidebar.tsx
@@ -0,0 +1,106 @@
+"use client";
+import { useState } from "react";
+import { SidebarItem } from "./SidebarItem";
+
+const tabs = ["Home", "Transfer", "Transactions", "P2P Transfer"];
+const hrefs = ["/dashboard", "/transfer", "/transactions", "/p2p"];
+const icons = [
+  <HomeIcon />,
+  <TransferIcon />,
+  <TransactionsIcon />,
+  <P2PTransferIcon />,
+];
+
+export const Sidebar = () => {
+  const [selected, setSelected] = useState(tabs[0]);
+  return (
+    <div>
+      {tabs.map((tab, idx) => (
+        <SidebarItem
+          href={hrefs[idx] || ""}
+          icon={icons[idx]}
+          title={tab}
+          selected={selected === tab}
+          setSelected={setSelected}
+          key={tab}
+        />
+      ))}
+    </div>
+  );
+};
+
+// Icons Fetched from https://heroicons.com/
+function HomeIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="w-7 h-7"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+      />
+    </svg>
+  );
+}
+function TransferIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="w-7 h-7"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5"
+      />
+    </svg>
+  );
+}
+
+function TransactionsIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="w-7 h-7"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+      />
+    </svg>
+  );
+}
+
+function P2PTransferIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="w-7 h-7"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+      />
+    </svg>
+  );
+}
--- apps/user-app/components/SidebarItem.tsx
+++ apps/user-app/components/SidebarItem.tsx
@@ -1,33 +1,43 @@
 "use client";
 import { usePathname, useRouter } from "next/navigation";
-import React from "react";
+import { motion } from "framer-motion";
+import React, { Dispatch, SetStateAction, useState } from "react";
 
 export const SidebarItem = ({
   href,
   title,
   icon,
+  selected,
+  setSelected,
 }: {
   href: string;
   title: string;
   icon: React.ReactNode;
+  selected: boolean;
+  setSelected: Dispatch<SetStateAction<string | undefined>>;
 }) => {
   const router = useRouter();
-  const pathname = usePathname();
-  const selected = pathname === href;
-
   return (
     <div
-      className={`flex ${selected ? "text-[#6a51a6]" : "text-slate-500"} cursor-pointer  p-2 pl-8`}
       onClick={() => {
+        setSelected(title);
         router.push(href);
       }}
+      className={`flex ${selected ? "text-textprimary  rounded-lg" : "text-texttertiary"} cursor-pointer py-6 justify-center items-center mx-4 relative`}
     >
-      <div className="pr-2">{icon}</div>
+      <div className={`pr-4 ${selected ? "z-10" : "z-0"}`}>{icon}</div>
       <div
-        className={`font-bold ${selected ? "text-[#6a51a6]" : "text-slate-500"}`}
+        className={`text-2xl font-bold ${selected ? "text-textprimary z-10" : "text-texttertiary"}`}
       >
         {title}
       </div>
+      {selected && (
+        <motion.span
+          layoutId="pill-tab"
+          transition={{ type: "spring", duration: 0.5 }}
+          className="absolute inset-0 z-0 bg-accentquaternary rounded-md"
+        ></motion.span>
+      )}
     </div>
   );
 };
